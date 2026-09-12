ebf8c49881c02564e130f75b18b3d2eccdac075c
john youssef <johnyoussefamin@gmail.com>
2026-09-13 00:49:58 +0300

    simon says project

---
+++ simon says arduino/code/code.ino
@@ -0,0 +1,24 @@
+// name all 4 led pins output in a list [0:3]
+// name all 4 botton pins (input out?) in a list [0:3]
+// name buzzer pin output
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+  // make an empty list {} name the_sequince
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  // make a random number from 0 to 3
+  // add this number to the_sequince
+
+  // light the leds in the sequince using list the_sequince
+  // wait for user to add the same sequince as the_sequince
+
+  // if user sucssed go back to line 15
+  // elif user failed or made an error, delete the_sequince and go back to line 15 with an empty sequince
+
+}
